7cab76e59ecefc7542762c88f7fa2967100abc5c
mu3-228 <mu-_-@ymail.ne.jp>
2026-06-22 01:16:49 +0900

    Add manual search feature

--- manuals/views.py
+++ manuals/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView,DetailView
+from django.db.models import Q
 from .models import Manual
 
 
@@ -8,6 +9,19 @@ class ManualListView(ListView):
     model = Manual
     template_name = "manuals/manual_list.html"
     context_object_name = "manuals"
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get("q")
+
+        if query:
+         queryset = queryset.filter(
+        Q(title__icontains=query) |
+        Q(category__icontains=query)
+    )
+
+        return queryset
+
 
 class ManualDetailView(DetailView):
     model = Manual
